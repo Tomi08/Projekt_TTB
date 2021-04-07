f120de7a2c728b794dd2758c66506e06fac7490d
Tivadar <verestivadar19@gmail.com>
2021-04-07 22:18:20 +0300

    asd

--- functions.h
+++ functions.h
@@ -4,5 +4,29 @@
 
 #ifndef PROJEKT_TTB_FUNCTIONS_H
 #define PROJEKT_TTB_FUNCTIONS_H
+typedef struct {
+    int ora;
+    int perc;
+    int masodperc;
+}IDOTARTAM;
+typedef struct {
+    int ev;
+    int honap;
+    int nap;
+}KIADAS;
+typedef struct {
+    IDOTARTAM lejatszasi_ido;
+    char *cime;
+    KIADAS megjelenes;
+    char *album;
+    char *eloado_neve;
+    char *feldolgozasok;
+    char *mood;
+    char *stilus;
+    int megtekintes;
+    int kedvelesek;
+    int ID;
 
+}ALMA;
+void beolvas(ALMA *zene);
 #endif //PROJEKT_TTB_FUNCTIONS_H
